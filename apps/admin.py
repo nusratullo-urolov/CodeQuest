83c137083bb97d15af489abd21762ece84c170f3
pythonuzbek <nusratullo3304@gmail.com>
2023-07-17 14:19:32 +0500

    change admin.py

--- apps/admin.py
+++ apps/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 from django.contrib.admin import ModelAdmin
 
-from apps.models import Category, Example, Answer
+from apps.models import Category, Example, Answer, Problems
 
 
 @admin.register(Category)
+class CategoryModelAdmin(ModelAdmin):
+    pass
+
+
+@admin.register(Problems)
 class CategoryModelAdmin(ModelAdmin):
     pass
 

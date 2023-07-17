93d85bb8a3f28d40dcb920782ccac47a476494ed
pythonuzbek <nusratullo3304@gmail.com>
2023-07-17 15:52:25 +0500

    add views.py

--- apps/views.py
+++ apps/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from apps.models import Problems
+from apps.serializers import ProblemsModelSerializer
 
-# Create your views here.
+
+class ProblemsView(ModelViewSet):
+    queryset = Problems.objects.all()
+    serializer_class = ProblemsModelSerializer

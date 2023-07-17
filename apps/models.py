5b596b119f3bd11c92cb7019f15d297be48171ef
pythonuzbek <nusratullo3304@gmail.com>
2023-07-17 14:08:52 +0500

    change models.py

--- apps/models.py
+++ apps/models.py
@@ -1,2 +1,32 @@
 from django.db import models
+from django.db.models import Model, TextChoices, CharField, IntegerField, ForeignKey, CASCADE
 
+
+class Category(Model):
+    title = CharField(max_length=255)
+
+
+class Problems(Model):
+    class Difficulty(TextChoices):
+        EASY = 'easy', 'Easy'
+        MEDIUM = 'medium', 'Medium',
+        HARD = 'hard', 'Hard'
+
+    title = CharField(max_length=255)
+    description = CharField(max_length=500)
+    type = CharField(max_length=6, choices=Difficulty.choices)
+    acceptance = IntegerField()
+    category = ForeignKey('apps.Category', CASCADE)
+
+
+class Example(Model):
+    input = CharField(max_length=255)
+    output = CharField(max_length=255, null=True)
+    explanation = CharField(max_length=255, null=True)
+    target = CharField(max_length=255, null=True)
+
+
+class Answer(Model):
+    output = CharField(max_length=255)
+    example = ForeignKey('apps.Example', CASCADE)
+    runtime = CharField(max_length=25)

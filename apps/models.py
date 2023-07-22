caa1aa59c94b62871a569b5254b79d25a63c9a90
pythonuzbek <nusratullo3304@gmail.com>
2023-07-22 19:39:49 +0500

    D

--- apps/models.py
+++ apps/models.py
@@ -1,23 +1,11 @@
 import json
 
 from django.contrib.postgres.fields import ArrayField
-from django.core.exceptions import ValidationError
-from django.db import models
 from django.db.models import Model, TextChoices, CharField, IntegerField, ForeignKey, CASCADE, BooleanField, \
     DateTimeField, SlugField, OneToOneField, JSONField
 from django.utils.text import slugify
 
 
-
-# class CustomJsonField(JSONField):
-#     def to_python(self, value):
-#         if value is None or isinstance(value, dict):
-#             return value
-#
-#         try:
-#             return json.loads(value)
-#         except (ValueError, TypeError):
-#             raise ValidationError('Invalid Json object')
 
 
 
@@ -38,7 +26,7 @@ class Problems(Model):
     description = CharField(max_length=500)
     type = CharField(max_length=6, choices=Difficulty.choices)
     category = ForeignKey('apps.Category', CASCADE)
-    # input = ArrayField(CustomJsonField())
+    input = JSONField()
     output = CharField(max_length=255)
     explanation = CharField(max_length=255, blank=True, null=True)
     created_at = DateTimeField(auto_now_add=True)
@@ -46,6 +34,11 @@ class Problems(Model):
 
     def __str__(self):
         return self.title
+
+
+    # def get_example(self):
+        # for k,v in Problems.objects.filter()
+            # return
 
 
 class Answer(Model):
